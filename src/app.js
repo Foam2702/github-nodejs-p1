dc6805f7d7c4bacfc7d41f1326d1fae9343158cb
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-11-05 13:36:45 +0700

    CreateServer

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const hbs = require("express-handlebars");
+const express = require("express");
+const app = express();
+const port = 3000;
+const morgan = require("morgan");
+const path = require("path");
+
+// app.use(express.static(path.join(__dirname, "public")));
+//app.use(morgan("combined"));
+//Template engine
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
