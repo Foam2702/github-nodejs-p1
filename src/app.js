7b63777282743b565ad55e3dcb47695b36ef5157
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-11-06 13:59:02 +0700

    complete calculator

--- src/app.js
+++ src/app.js
@@ -5,26 +5,47 @@ const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
-
+const fs = require("fs");
+app.use(bodyparser.urlencoded({ extended: true }));
 //app.use(morgan("combined"));
 //Template engine
 app.engine(
   "hbs",
   hbs.engine({
     extname: ".hbs",
+    //defaultLayout: "layouts",
+    // layoutsDir: __dirname + "/views/layouts",
   })
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
+
 app.get("/", (req, res) => {
   res.render("home");
-
 });
 
-app.post("/calculator", function (req, res) {
-  res.render('other');
-})
-
+app.post("/", function (req, res) {
+  let rs = 0;
+  const x = parseFloat(req.body.x);
+  const y = parseFloat(req.body.y);
+  switch (req.body.op) {
+    case "+":
+      rs = x + y;
+      break;
+    case "-":
+      rs = x - y;
+      break;
+    case "x":
+      rs = x * y;
+      break;
+    case ":":
+      rs = x / y;
+      break;
+  }
+  res.render("home", {
+    Result: rs,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
